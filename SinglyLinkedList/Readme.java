33a6d38176524cbb7454f4a61e966371f5e4bf0b
Yatin Chaudhari <c.yatin727@gmail.com>
2023-07-26 15:02:32 +0530

    Update Readme.java

--- SinglyLinkedList/Readme.java
+++ SinglyLinkedList/Readme.java
@@ -5,6 +5,7 @@ public class Readme {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	  System.out.println(" All The Implemention Of SinglyLinkedList Are Included In this Folder : ");
 	}
 
 }
