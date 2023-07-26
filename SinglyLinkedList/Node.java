62b41569877a2edd640e18c1c05f6a53f9981cf3
Yatin Chaudhari <c.yatin727@gmail.com>
2023-07-26 14:59:38 +0530

    Add files via upload

---
+++ SinglyLinkedList/Node.java
@@ -0,0 +1,21 @@
+package SinglyLinkedList;
+import java.util.*;
+
+// Creating A Node Class for All LinkedList
+public class Node {
+	
+	public int data;
+	public Node next;
+	
+	public Node (int data) {
+	     this.data = data;
+	     this.next = null;
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
